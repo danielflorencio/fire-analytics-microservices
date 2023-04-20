0e73716c012a31ba7401ef165fd85d1a5be46408
danielflorencio <danielflorencio20@gmail.com>
2023-04-19 22:49:14 -0300

    made the new versions of the preview API for six months and a year.

--- src/main/java/com/example/myProject/MyProject.java
+++ src/main/java/com/example/myProject/MyProject.java
@@ -5,10 +5,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.myProject.data.expensesData;
 import com.example.myProject.models.DayData;
@@ -63,8 +60,7 @@ public class MyProject {
         }
       }    
       // System.out.println("daysData post second: " + daysData);
-      
-      
+       
       Double monthPreview = 0.0;
       Double averageDailyGain = daysData.get(0).getIncomes();
       Double averageDailyLoss = daysData.get(0).getExpenses();
@@ -79,19 +75,100 @@ public class MyProject {
     }
 
 
+    public Double calculateSixMonthsPreview(String userId){      
+      List<Date> sortedDates = new ArrayList<>(getSortedDates(expensesData.expenses));
+      // System.out.println("Sorted Dates: " + sortedDates);
+      // Get the data from the database. In this case, we're gonna get it from another java file.  
+      List<DayData> daysData = new ArrayList<>();
+      for(int i = 0; i < sortedDates.size(); i++){
+        DayData newDay = new DayData(sortedDates.get(i));
+        daysData.add(newDay);
+      }
+      // System.out.println("daysData post first FOR: " + daysData);
+      for(int i = 0; i < sortedDates.size(); i++){
+        for(int n = 0; n < expensesData.expenses.size(); n++){
+          if(sortedDates.get(i).equals(expensesData.expenses.get(n).getDate())){
+            if(expensesData.expenses.get(n).getCategory().equals("salary") || expensesData.expenses.get(n).getCategory().equals("Salary")){
+              daysData.get(i).addIncomeValue(expensesData.expenses.get(n).value);              
+            } else{
+              daysData.get(i).addExpenseValue(expensesData.expenses.get(n).value);
+            }
+          }
+        }
+      }    
+      // System.out.println("daysData post second: " + daysData);
+       
+      Double sixMonthsPreview = 0.0;
+      Double averageDailyGain = daysData.get(0).getIncomes();
+      Double averageDailyLoss = daysData.get(0).getExpenses();
+      for(int i = 1; i < daysData.size(); i++){
+        averageDailyGain = (averageDailyGain + daysData.get(i).getIncomes())/2;
+        averageDailyLoss = (averageDailyLoss + daysData.get(i).getExpenses())/2;
+      }
+      for(int i = 0; i < 182; i ++){
+        sixMonthsPreview = sixMonthsPreview + (averageDailyGain - averageDailyLoss);
+      }
+      return sixMonthsPreview;
+    }
 
-    // https://www.youtube.com/watch?v=X2hjlquVess how to fix the cors shit
-    // @RequestMapping(value = "/getUserMonthPreview", method = RequestMethod.POST)
-    // @RequestMapping("/getUserMonthPreview")
-    
-    @GetMapping("/getUserMonthPreview")
+    public Double calculateYearPreview(String userId){      
+      List<Date> sortedDates = new ArrayList<>(getSortedDates(expensesData.expenses));
+      // System.out.println("Sorted Dates: " + sortedDates);
+      // Get the data from the database. In this case, we're gonna get it from another java file.  
+      List<DayData> daysData = new ArrayList<>();
+      for(int i = 0; i < sortedDates.size(); i++){
+        DayData newDay = new DayData(sortedDates.get(i));
+        daysData.add(newDay);
+      }
+      // System.out.println("daysData post first FOR: " + daysData);
+      for(int i = 0; i < sortedDates.size(); i++){
+        for(int n = 0; n < expensesData.expenses.size(); n++){
+          if(sortedDates.get(i).equals(expensesData.expenses.get(n).getDate())){
+            if(expensesData.expenses.get(n).getCategory().equals("salary") || expensesData.expenses.get(n).getCategory().equals("Salary")){
+              daysData.get(i).addIncomeValue(expensesData.expenses.get(n).value);              
+            } else{
+              daysData.get(i).addExpenseValue(expensesData.expenses.get(n).value);
+            }
+          }
+        }
+      }    
+      // System.out.println("daysData post second: " + daysData);
+       
+      Double yearPreview = 0.0;
+      Double averageDailyGain = daysData.get(0).getIncomes();
+      Double averageDailyLoss = daysData.get(0).getExpenses();
+      for(int i = 1; i < daysData.size(); i++){
+        averageDailyGain = (averageDailyGain + daysData.get(i).getIncomes())/2;
+        averageDailyLoss = (averageDailyLoss + daysData.get(i).getExpenses())/2;
+      }
+      for(int i = 0; i < 365; i ++){
+        yearPreview = yearPreview + (averageDailyGain - averageDailyLoss);
+      }
+      return yearPreview;
+    }
+
+    @GetMapping("/getMonthPreview")
     @CrossOrigin(origins = "http://localhost:5173/") 
-    // @ResponseBody
     public ResponseEntity<Double> getMonthPreview(@RequestParam(value = "userId", defaultValue = "0") String userId) {
       Double monthPreview = calculateMonthPreview(userId);
       HttpHeaders headers = new HttpHeaders();
       headers.add("Status", "ok");
-      // headers.add("Access-Control-Allow-Origin", "*");
+      return ResponseEntity.ok().headers(headers).body(monthPreview);    
+    }
+    @GetMapping("/getSixMonthsPreview")
+    @CrossOrigin(origins = "http://localhost:5173/") 
+    public ResponseEntity<Double> getSixMonthsPreview(@RequestParam(value = "userId", defaultValue = "0") String userId) {
+      Double monthPreview = calculateSixMonthsPreview(userId);
+      HttpHeaders headers = new HttpHeaders();
+      headers.add("Status", "ok");
+      return ResponseEntity.ok().headers(headers).body(monthPreview);    
+    }
+    @GetMapping("/getUserYearPreview")
+    @CrossOrigin(origins = "http://localhost:5173/") 
+    public ResponseEntity<Double> getYearPreview(@RequestParam(value = "userId", defaultValue = "0") String userId) {
+      Double monthPreview = calculateYearPreview(userId);
+      HttpHeaders headers = new HttpHeaders();
+      headers.add("Status", "ok");
       return ResponseEntity.ok().headers(headers).body(monthPreview);    
     }
 }
