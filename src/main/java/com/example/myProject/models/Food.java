264973be5de9be06feb6f99c726fa48ae63ad97c
danielflorencio <danielflorencio20@gmail.com>
2023-06-27 12:09:14 -0300

    Configured Lombok to work with the project.

--- src/main/java/com/example/myProject/models/Food.java
+++ src/main/java/com/example/myProject/models/Food.java
@@ -5,8 +5,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity(name = "foods")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Food {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
