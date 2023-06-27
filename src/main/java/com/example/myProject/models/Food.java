d55735f2ff6bb7034b6f0520fda32de3d42b722a
danielflorencio <danielflorencio20@gmail.com>
2023-06-27 12:00:24 -0300

    fixed the non-existent relation bug.

--- src/main/java/com/example/myProject/models/Food.java
+++ src/main/java/com/example/myProject/models/Food.java
@@ -7,7 +7,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity(name = "foods")
-@Table(name = "foods")
 public class Food {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
