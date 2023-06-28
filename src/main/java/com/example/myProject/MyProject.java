3ae17a24aee9c7dfc9b9280f65e22d1293da5871
danielflorencio <danielflorencio20@gmail.com>
2023-06-28 04:33:12 -0300

    Saving code changes.

--- src/main/java/com/example/myProject/MyProject.java
+++ src/main/java/com/example/myProject/MyProject.java
@@ -2,11 +2,13 @@ package com.example.myProject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.myProject.repositories.ExpenseRepository;
 @SpringBootApplication
 @RestController
 public class MyProject {
     public static void main(String[] args) {
       SpringApplication.run(MyProject.class, args);
     }
-
+    
 }
---
+++ src/main/java/com/example/myProject/controllers/ExpensesController.java
@@ -0,0 +1,32 @@
+package com.example.myProject.controllers;
+import com.example.myProject.data.expensesData;
+import com.example.myProject.models.Expense;
+import com.example.myProject.repositories.ExpenseRepository;
+
+import org.apache.catalina.connector.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("expenses")
+public class ExpensesController {
+    
+    @Autowired
+    private ExpenseRepository expenseRepository;
+
+    @GetMapping("saveExpenses")
+
+    public ResponseEntity<Double> saveExpenses() {
+        for (Expense expense : expensesData.expenses) {
+            expenseRepository.save(expense);
+        }
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Status", "ok");
+        return ResponseEntity.ok().headers(headers).body(1d); 
+    }
+}
